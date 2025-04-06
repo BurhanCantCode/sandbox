f0743740679613e4937ac24aec145be35f1639a1
BurhanCantCode <burhanuddinkhatri@gmail.com>
2025-04-06 05:00:50 +0500

    removed unecassary folder

--- Burhanthegoat/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  // Add trailing slash to ensure consistent routing
-  trailingSlash: true,
-};
-
-module.exports = nextConfig; 
